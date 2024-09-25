b54526b36e41345402cc491247196f9a4155c537
sauveerk <sauveer.ketan@gmail.com>
2024-09-25 10:14:16 +0530

    Set up Terraform EC2 provisioning

---
+++ backend.tf
@@ -0,0 +1,11 @@
+#Configure AWS S3 bucket as remote backend 
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-backend-xdf3489xz"
+    key            = "ec2-provision/terraform-ec2.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "tfstate-locking"
+  }
+}
